6f0e01f3c9a716c8444fb0dc727ae3f60daf86b1
Aditya Kalisetty <aditya.kalisetty@protonmail.com>
2025-11-05 20:13:14 -0600

    Add CORS configuration for custom domain

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,19 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://adityakalisetty.github.io',
+        'https://omnigraphyshop.com',
+        'https://www.omnigraphyshop.com'
+      ],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
